1f60685c4b7fb96045df1924f66908a2e43b4185
ShivaniGanatra <shivaniganatra@live.com>
2024-08-26 14:57:13 +0100

    Created an intructions sliding tab and added google fonts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,26 @@
 //Query Selectors
+const nav = document.querySelector(".nav-container");
+
+if (nav) {
+  const toggle = nav.querySelector(".nav-toggle");
+  
+  if (toggle) {
+    toggle.addEventListener("click", () => {
+      if (nav.classList.contains("is-active")) {
+        nav.classList.remove("is-active");
+      }
+      else {
+        nav.classList.add("is-active");
+      }
+    });
+    
+    nav.addEventListener("blur", () => {
+      nav.classList.remove("is-active");
+    });
+  }
+}
+
+
 const letters = document.querySelectorAll<HTMLButtonElement>(
     ".letters-section__letter"
 );
