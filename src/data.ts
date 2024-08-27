7f07a49965be16ebdd542e98a83d4623b0729be1
ShivaniGanatra <shivaniganatra@live.com>
2024-08-27 19:06:30 +0100

    Moved all data to a seperate data.ts file and exported them to the main.ts file, Reused the getRandomStringFromArray function to retieve a random gif from winning and loosing gifs arrays

---
+++ src/data.ts
@@ -0,0 +1,63 @@
+export const taylorArray: string[] = [
+    "thirteen",
+    "august",
+    "lover",
+    "december",
+    "style",
+    "folklore",
+    "evermore",
+    "midnights",
+    "alltoowell",
+    "lovestory",
+    "archer",
+    "fearless",
+    "enchanted",
+];
+
+export const taylorClues: string[] = [
+    "Are you even a Swiftie if you dont know Taylors favourite number",
+    "This song is featured in Folklore",
+    "The perfect wedding song",
+    "This song was inspired by Taylor Lautner",
+    "Someone who is chic and fashionable is someone that has a good sense of ......",
+    "One of Taylors more cottagecore albums",
+    "One of Taylors more cottagecore albums",
+    "Think purple",
+    "Theres a ten minute version of this song",
+    "People often propose with this song",
+    "This song is featured in Lover",
+    "One of Taylors earlier albums",
+    "A song featured on Speak Now",
+];
+
+export const hangmanArray: string[] = [
+    `<img class="hangman__image" src="./src/images/desktop images/hangman desktop-1.jpg" alt="">`,
+    `<img class="hangman__image" src="./src/images/desktop images/hangman desktop-2.jpg" alt="">`,
+    `<img class="hangman__image" src="./src/images/desktop images/hangman desktop-3.jpg" alt="">`,
+    `<img class="hangman__image" src="./src/images/desktop images/hangman desktop-4.jpg" alt="">`,
+    `<img class="hangman__image" src="./src/images/desktop images/hangman desktop-5.jpg" alt="">`,
+    `<img class="hangman__image" src="./src/images/desktop images/hangman desktop-6.jpg" alt="">`,
+    `<img class="hangman__image" src="./src/images/desktop images/hangman desktop-7.jpg" alt="">`,
+    `<img class="hangman__image" src="https://media.tenor.com/FFurq2xQgeoAAAAM/taylor-swift-cheers.gif" alt="">`,
+    `<img class="hangman__image" src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmVvZzhxeGV6YWQyenFnOXkzbjd2ZzlmZWs5NGl1cG8xZ2w1aXl1diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11wKUA9rPyvyuI/giphy.webp" alt="">`,
+];
+// export default {taylorArray, taylorClues};
+
+export const loosingGifsArray:string[] = [
+    `<img class="hangman__image" src="https://media.tenor.com/FFurq2xQgeoAAAAM/taylor-swift-cheers.gif" alt="Taylor saying dont hate the player hate the game">`,
+    `<img class="hangman__image" src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTF4NmI4cGJqa3VjMXMyNTNxbGp4b2wxeGdrdGx4b2ZkaDZyOTVndiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jbcJ7iSy9pPkxKmqaI/giphy.webp" alt="Taylor saying dont hate the player hate the game">`,
+    `<img class="hangman__image" src="https://i.giphy.com/3rgXBt9KoMxnpgB2RG.webp" alt="Taylor saying dont hate the player hate the game">`,
+    `<img class="hangman__image" src="https://i.giphy.com/a4gLl8darNId2.webp" alt="Taylor saying dont hate the player hate the game">`,
+    `<img class="hangman__image" src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExajlzNHRxMXljNHB0ZXZ0YzQwM3hodDA2OGE3aHBoMmYyOGE5eHJ6dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6gg6NkLsub7EzIgE/giphy.webp" alt="Taylor saying dont hate the player hate the game">`,
+    
+
+
+]
+
+export const winningGifsArray :string[] = [
+    `<img class="hangman__image" src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmVvZzhxeGV6YWQyenFnOXkzbjd2ZzlmZWs5NGl1cG8xZ2w1aXl1diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11wKUA9rPyvyuI/giphy.webp" alt="Taylor celebrating">`,
+    `<img class="hangman__image" src="https://assets.teenvogue.com/photos/55b271a774dfbc51751ef24a/master/w_1600,c_limit/taydance4.gif" alt=Taylor celebrating">`,
+    `<img class="hangman__image" src="https://assets.teenvogue.com/photos/55b271a474dfbc51751ef240/master/w_1600,c_limit/taydance5.gif" alt="Taylor celebrating">`,
+    `<img class="hangman__image" src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3R1anJkcDR4NW8zbGpvbzVjaHF1NDNzemlmMWpzYXQ2OGN5MTZtZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hRFhL1ZFrSwpOg4HpS/giphy.webp" alt="Taylor celebrating">`,
+    `<img class="hangman__image" src="https://i.giphy.com/cyBFFJ9kErbuw8xNXc.webp" alt="Taylor celebrating">`,
+]
