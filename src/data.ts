c1a76491edad7c87fb252271a06ed12eefc7e7f1
ShivaniGanatra <shivaniganatra@live.com>
2024-08-28 15:17:19 +0100

    Adjusted gifs styling to ensure it fir into hangman container

--- src/data.ts
+++ src/data.ts
@@ -43,22 +43,23 @@ export const hangmanArray: string[] = [
 ];
 
 export const loosingGifsArray:string[] = [
-    `<img class="hangman__image" src="https://media.tenor.com/FFurq2xQgeoAAAAM/taylor-swift-cheers.gif" alt="Taylor saying dont hate the player hate the game">`,
-    `<img class="hangman__image" src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTF4NmI4cGJqa3VjMXMyNTNxbGp4b2wxeGdrdGx4b2ZkaDZyOTVndiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jbcJ7iSy9pPkxKmqaI/giphy.webp" alt="Taylor saying dont hate the player hate the game">`,
-    `<img class="hangman__image" src="https://i.giphy.com/3rgXBt9KoMxnpgB2RG.webp" alt="Taylor saying dont hate the player hate the game">`,
-    `<img class="hangman__image" src="https://i.giphy.com/a4gLl8darNId2.webp" alt="Taylor saying dont hate the player hate the game">`,
-    `<img class="hangman__image" src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExajlzNHRxMXljNHB0ZXZ0YzQwM3hodDA2OGE3aHBoMmYyOGE5eHJ6dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6gg6NkLsub7EzIgE/giphy.webp" alt="Taylor saying dont hate the player hate the game">`,
+    `<img class="hangman__GIF" src="https://media.tenor.com/FFurq2xQgeoAAAAM/taylor-swift-cheers.gif" alt="Taylor saying dont hate the player hate the game">`,
+    `<img class="hangman__GIF" src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTF4NmI4cGJqa3VjMXMyNTNxbGp4b2wxeGdrdGx4b2ZkaDZyOTVndiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jbcJ7iSy9pPkxKmqaI/giphy.webp" alt="Taylor saying dont hate the player hate the game">`,
+    `<img class="hangman__GIF" src="https://i.giphy.com/3rgXBt9KoMxnpgB2RG.webp" alt="Taylor saying dont hate the player hate the game">`,
+    `<img class="hangman__GIF" src="https://i.giphy.com/a4gLl8darNId2.webp" alt="Taylor saying dont hate the player hate the game">`,
+    `<img class="hangman__GIF" src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExajlzNHRxMXljNHB0ZXZ0YzQwM3hodDA2OGE3aHBoMmYyOGE5eHJ6dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6gg6NkLsub7EzIgE/giphy.webp" alt="Taylor saying dont hate the player hate the game">`,
 ]
 
 export const winningGifsArray :string[] = [
-    `<img class="hangman__image" src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmVvZzhxeGV6YWQyenFnOXkzbjd2ZzlmZWs5NGl1cG8xZ2w1aXl1diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11wKUA9rPyvyuI/giphy.webp" alt="Taylor celebrating">`,
-    `<img class="hangman__image" src="https://assets.teenvogue.com/photos/55b271a774dfbc51751ef24a/master/w_1600,c_limit/taydance4.gif" alt=Taylor celebrating">`,
-    `<img class="hangman__image" src="https://assets.teenvogue.com/photos/55b271a474dfbc51751ef240/master/w_1600,c_limit/taydance5.gif" alt="Taylor celebrating">`,
-    `<img class="hangman__image" src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3R1anJkcDR4NW8zbGpvbzVjaHF1NDNzemlmMWpzYXQ2OGN5MTZtZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hRFhL1ZFrSwpOg4HpS/giphy.webp" alt="Taylor celebrating">`,
-    `<img class="hangman__image" src="https://i.giphy.com/cyBFFJ9kErbuw8xNXc.webp" alt="Taylor celebrating">`,
+    `<img class="hangman__GIF" src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmVvZzhxeGV6YWQyenFnOXkzbjd2ZzlmZWs5NGl1cG8xZ2w1aXl1diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11wKUA9rPyvyuI/giphy.webp" alt="Taylor celebrating">`,
+    `<img class="hangman__GIF" src="https://assets.teenvogue.com/photos/55b271a774dfbc51751ef24a/master/w_1600,c_limit/taydance4.gif" alt=Taylor celebrating">`,
+    `<img class="hangman__GIF" src="https://assets.teenvogue.com/photos/55b271a474dfbc51751ef240/master/w_1600,c_limit/taydance5.gif" alt="Taylor celebrating">`,
+    `<img class="hangman__GIF" src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3R1anJkcDR4NW8zbGpvbzVjaHF1NDNzemlmMWpzYXQ2OGN5MTZtZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hRFhL1ZFrSwpOg4HpS/giphy.webp" alt="Taylor celebrating">`,
+    `<img class="hangman__GIF" src="https://i.giphy.com/cyBFFJ9kErbuw8xNXc.webp" alt="Taylor celebrating">`,
 ]
 
-export const loosingPhrases = [
+//these are all lyrics turned into phrases if someone has won or not
+export const loosingPhrases:string[] = [
     `Were you send by someone you wanted me dead? The answer was actually `,
     `Come on, come on, don't leave me like this. I thought you figured out the answer was `,
     `Now I'm down bad crying at the gym. The answer was `,
@@ -66,12 +67,11 @@ export const loosingPhrases = [
     `But I got a blank(/missing) space baby. The answers name was  `,
     `No body, no crime, no answer either. The was answer was actually `,
     `Shake it off! The answer was `,
-    `Said, "I'm fine, but it wasn't true. You didnt get the answer `
+    `Said, "I'm fine, but it wasn't true. You didnt get the answer. It was  `
 ]
 
-export const winningPhrases = [
+export const winningPhrases:string[] = [
     `Karma is my boyfriend, Karma is a god, Karma is the reason you got the answer `,
     `I don't know about you. But I'm feeling twenty-two. I'm saved because you pulled through. The answer was `,
     `I'm wonderstruck, blushing all the way home because you got the answer `
-
 ]
